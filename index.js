3b2ac599535bbc0ef6e62ee094cb03c2982a9a8d
MasumaMohoni2028 <121331335+MasumaMohoni2028@users.noreply.github.com>
2023-10-29 13:51:10 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+   
+    var myButton = document.getElementById("myButton");
+
+    
+    myButton.addEventListener("click", function() {
+        // When the button is clicked, show an alert
+        alert("Thanks for visiting!");
+    });
+});
